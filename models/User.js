5bdd378fc492bca27c93b8e235e71ac4b8eac58d
sithuch <chhemsithuch1999@gmail.com>
2025-03-07 16:31:36 +0700

    create crudd api end-points about role

--- models/User.js
+++ models/User.js
@@ -1,8 +1,20 @@
 const mongoose = require("mongoose");
+// import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     age: Number,
+    // role :{
+    //     type : mongoose.Schema.Types.ObjectId,
+    //     ref : "Role"
+    // }
 });
+
+// const RoleSchema = new mongoose.Schema({
+//     name : String,
+//     description : String,
+//     permissions : String,
+// })
 module.exports = mongoose.model("User", userSchema);
+// module.exports = mongoose.model("Role", RoleSchema);
--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const roleController = require("../controllers/roleController");
 
-router.get("/", userController.getUsers);
-router.get("/:id", userController.getUser);
-router.post("/", userController.createUser);
-router.put("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUser);
+// Routes for users
+router.get("users/", userController.getUsers);
+router.get("users/:id", userController.getUser);
+router.post("users/", userController.createUser);
+router.put("users/:id", userController.updateUser);
+router.delete("users/:id", userController.deleteUser);
 
+// Routes for roles
+router.post("/roles", roleController.createRole);
+router.get("/roles", roleController.getRoles);
+router.get("/roles/:id", roleController.getRole);
+router.put("/roles/:id", roleController.updateRole);
+router.delete("/roles/:id", roleController.deleteRole);
 module.exports = router;
